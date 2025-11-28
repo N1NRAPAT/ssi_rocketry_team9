d8a0687e94e81aab94b39f53cdda71137ca8f48b
N1NRAPAT <ninninrapat@gmail.com>
2025-11-28 16:32:00 +0000

    Flashing LED by press 'x

--- src/main.c
+++ src/main.c
@@ -11,25 +11,41 @@ int main()
 {
     stdio_init_all();
     // Init PCA9685
-    pca9685_init();
+   
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    int servo[4] = {0, 1, 2, 3}; // Servo channels
+    bool running = false;
+
+    printf("Pico Ready!\n");
+    printf("Press 's' to START, 'x' to STOP\n");
 
     while (1) {
-        // Test_case_1 : Move a servo from 0° to 180° 
-       for(int i = 0; i < 4 ; i++) {
-           set_servo_angle(servo[i], 0);
-        }
-        sleep_ms(2000);
+        int c = getchar_timeout_us(0); // non-blocking read
 
-        for(int i = 0; i < 4 ; i++) {
-            set_servo_angle(servo[i], 180);
+        if (c != PICO_ERROR_TIMEOUT) {
+            if (c == 's') {
+                running = true;
+                printf("START command received!\n");
+            }
+            if (c == 'x') {
+                running = false;
+                printf("STOP command received!\n");
+                gpio_put(PICO_DEFAULT_LED_PIN, 0);
+            }
         }
-        sleep_ms(2000);
-        for (int i = 0; i < 4; i++) {
-            set_servo_angle(servo[i], 180);
+
+        if (running) {
+            gpio_put(PICO_DEFAULT_LED_PIN, 1);
+            printf("LED ON\n");
+            sleep_ms(300);
+
+            gpio_put(PICO_DEFAULT_LED_PIN, 0);
+            printf("LED OFF\n");
+            sleep_ms(300);
+        } else {
+            sleep_ms(50);
         }
-        sleep_ms(2000);
     }
 }
 
