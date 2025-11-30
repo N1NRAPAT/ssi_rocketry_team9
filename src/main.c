3188107228de43b1ad2a152dad9b066d28a9eab8
N1NRAPAT <ninninrapat@gmail.com>
2025-11-30 22:07:34 +0000

    BEFORE MESSING PROGRAM

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-#include "devices/servo.h" // servo
+// #include "devices/servo.h" // servo
 #include "devices/imu.h" // imu
 
 
@@ -10,8 +10,9 @@
 int main()
 {
     stdio_init_all();
-    // Init PCA9685
    
+    // ------- LED Flashing program ---------
+
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
@@ -20,33 +21,44 @@ int main()
     printf("Pico Ready!\n");
     printf("Press 's' to START, 'x' to STOP\n");
 
+    // Flashing LED to Pico when press 's' and stop flash when press 'x'
+
+
+
+
     while (1) {
-        int c = getchar_timeout_us(0); // non-blocking read
 
-        if (c != PICO_ERROR_TIMEOUT) {
-            if (c == 's') {
-                running = true;
-                printf("START command received!\n");
+        int count = 0 ; 
+        while(count < 5){
+            int c = getchar_timeout_us(0); // non-blocking read
+            if (c != PICO_ERROR_TIMEOUT) {
+                if (c == 's') {
+                    running = true;
+                    printf("START command received!\n");
+                }
+                if (c == 'x') {
+                    running = false;
+                    count++ ; 
+                    printf("STOP command received!\n");
+                    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+                }
             }
-            if (c == 'x') {
-                running = false;
-                printf("STOP command received!\n");
+
+            if (running) {
+                gpio_put(PICO_DEFAULT_LED_PIN, 1);
+                printf("LED ON\n");
+                sleep_ms(300);
+
                 gpio_put(PICO_DEFAULT_LED_PIN, 0);
+                printf("LED OFF\n");
+                sleep_ms(300);
+            } else {
+                sleep_ms(50);
             }
         }
 
-        if (running) {
-            gpio_put(PICO_DEFAULT_LED_PIN, 1);
-            printf("LED ON\n");
-            sleep_ms(300);
-
-            gpio_put(PICO_DEFAULT_LED_PIN, 0);
-            printf("LED OFF\n");
-            sleep_ms(300);
-        } else {
-            sleep_ms(50);
-        }
     }
+
 }
 
 
