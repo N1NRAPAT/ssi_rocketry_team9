84dbce190fdab3bf5b5544bcd816b4cbd0845cf3
N1NRAPAT <ninninrapat@gmail.com>
2025-12-04 17:07:32 +0000

    Selection test run in C

--- src/main.c
+++ src/main.c
@@ -2,14 +2,22 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "devices/imu.h" // imu
+
+#include "devices/barometric_sensor/MS5611.h" // Baro
+#include "devices/barometric_sensor/altitude.h" // Altitude
 // #include "data/sdcard.h"
 
+typedef enum {
+    MODE_NONE = 0,
+    MODE_IMU,
+    MODE_BARO,
+    MODE_BOTH
+} test_mode_t;
 
 int main()
 {
     stdio_init_all();
     sleep_ms(3000); // Wait 3 s
-    printf("Ready!\n");
     // ------- LED Flashing program ---------
 
     gpio_init(PICO_DEFAULT_LED_PIN);
@@ -24,13 +32,19 @@ int main()
     gpio_pull_up(4); //Register pin 4
     gpio_pull_up(5); //Resgister pin 5 
 
+    // initialize sensors
     mpu6050_init();
-    // Program
-    // printf("PICO and MPU6050 are ready\n");
+    MS5611_init();
+
+    printf("Pico ready, Command: \n");
+    printf("i = imu sensor run test \n");
+    printf("b = barometer sensor run test \n");
+    printf("a = all sensors run \n");
+    printf("n = none (Break)\n");
+
     imu_data_t imu;
-
-    // Flashing LED to Pico when press 's' and stop flash when press 'x'
-
+    test_mode_t MODE_NONE;
+    int running = 0 ;
     absolute_time_t start = get_absolute_time(); //Set clock timer 
     bool led_state = false;
     uint32_t last_led_toggle = 0;
@@ -38,13 +52,58 @@ int main()
 
     while (1) {
 
+        int ch = getchar_timeout_us(0); //Get char and non-blocking programming 
         /*
+            Assign task mode to Pico like switch fundtion key so I don't need
+            to run code once again 
+        */
+        if (ch != PICO_ERROR_TIMEOUT) {
+            if (ch == 'i' || ch == 'I')  mode = MODE_IMU;
+            else if (ch == 'b'|| ch =='B') mode = MODE_BARO;
+            else if (ch == 'a'|| ch == 'A') mode = MODE_BOTH;
+            else if (ch == 'n'|| ch == 'N') mode = MODE_NONE;
+
+            printf("Mode change: %d\n", mode);
+        }
+
+        switch (mode)
+        {
+        case MODE_IMU:
+            mpu6050_read_all(&imu);
+            printf("Ax:%d,Ay:%d,Az:%d,Gx:%d,Gy:%d,Gz:%d\n",
+                imu.ax, imu.ay, imu.az,
+                imu.gx, imu.gy, imu.gz )
+            break;
+
+        case MODE_BARO:
+            float p = MS5611_read_pressure();
+            float t = MS5611_read_temperature();
+            printf("BARO | P:%.2f Pa  T:%.2f C\n", p, t);
+            break;
+
+        case MODE_BOTH:
+            mpu6050_read_all(&imu);
+            float p = MS5611_read_pressure();
+            float t = MS5611_read_temperature();
+            printf("ALL | AX:%d AY:%d AZ:%d  GX:%d GY:%d GZ:%d  P:%.2f Pa T:%.2f C\n",
+                    imu.ax, imu.ay, imu.az,
+                    imu.gx, imu.gy, imu.gz,
+                    p, t);
+            break;
+    
+        case MODE_NONE:    
+        default:
+            break;
+        }
+        
+        sleep_ms(200) ; 
+
+        /* Week 3 : 
             Testing 1 : IMU ouput value + basic simulation 
             Testing 2 : IMU output in 30s to 1 min to SD-Card then convert to .csv file and save in new folder 
         */
-        
+        while (running != 0){
         // elapsed time in milliseconds
-        /*
         int32_t now = to_ms_since_boot(get_absolute_time());
         if (now >= 30000 && now <= 60000){
             //Read all imu value ax, ay, az / gx, gy, gz
@@ -70,18 +129,8 @@ int main()
         else {
             gpio_put(PICO_DEFAULT_LED_PIN, 0);
             sleep_ms(20);
+            }
         }
-   */
-
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        printf("LED ON\n");
-        sleep_ms(500);
-        
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        printf("LED OFF\n");
-        sleep_ms(500);
-
-
     }
 
    return 0 ; 
